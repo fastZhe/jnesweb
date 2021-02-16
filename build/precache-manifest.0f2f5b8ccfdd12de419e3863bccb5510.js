9323d694c89c553e6e6dea4e854b340cc4cecf64
Hzhe <526910607@qq.com>
2021-02-16 11:34:18 +0800

    gh-pages update

---
+++ build/precache-manifest.0f2f5b8ccfdd12de419e3863bccb5510.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "626c496d8b8f7293859d72d0d1ee54cd",
+    "url": "/jsnes-web/index.html"
+  },
+  {
+    "revision": "8a8fb539658abb0287eb",
+    "url": "/jsnes-web/static/css/main.71122778.chunk.css"
+  },
+  {
+    "revision": "e319e3310f2efc6426e7",
+    "url": "/jsnes-web/static/js/2.1c6eeed0.chunk.js"
+  },
+  {
+    "revision": "ce788a1fa508dab3627efbe6528a9a6f",
+    "url": "/jsnes-web/static/js/2.1c6eeed0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a8fb539658abb0287eb",
+    "url": "/jsnes-web/static/js/main.57d38e71.chunk.js"
+  },
+  {
+    "revision": "fd848a91fab14049e0bc",
+    "url": "/jsnes-web/static/js/runtime-main.8273e804.js"
+  }
+]);
